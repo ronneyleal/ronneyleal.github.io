8c560348f10a54b6a657869461d8416a255209f6
Ronney Leal <ronneyharper@gmail.com>
2024-03-22 18:14:01 -0300

    v.1 done

---
+++ js/script.js
@@ -0,0 +1,65 @@
+// Toggle icon menu
+let menuIcon = document.querySelector('header #menu-icon');
+let navbar = document.querySelector('.navbar');
+let header = document.querySelector('header');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    header.classList.toggle('header-menu');
+};
+
+
+// scroll section active
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    // header-active
+    let header = document.querySelector('header');
+
+    header.classList.toggle('header-active', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+    header.classList.remove('header-menu');
+};
+
+// scroll reveal
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .sobre-box img, .heading', {origin: 'top'});
+ScrollReveal().reveal('.home-animation, .sobre-container, .servicos-container, .projetos-box, .faq-accordion-item', {origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .sobre-box h3, .left-container .tl-text-box h3, .left-container .tl-text-box p, .footer-text', {origin: 'left'});
+ScrollReveal().reveal('.home-content p, .sobre-box p, .right-container .tl-text-box h3, .right-container .tl-text-box p, .footer-iconTop', {origin: 'right'});
+
+// typed js 
+const typed = new Typed ('.multiple-text', {
+    strings: ['Decifrando dados!', 'Orientando visões!', 'Impulsionando resultados!'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
